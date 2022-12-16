7d4d0ee3f692b0f4e52064e837a631d6e1f636a1
Victor Lima <limaeriko48@gmail.com>
2022-12-16 18:42:14 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// get filter element
+// get cards elements
+
+// add input event for the filter element
+
+// filter function
+  // if the filter is not empty
+    // for each card of cards
+      // get card heading (title)
+      // tranform to lower case
+      // transform filter text to lower case
+      // if card title does not include the filter text
+        // hide the card element
+      // else
+        // unhide the card element
+  // else
+    // for each card of cards
+      // unhide the card element
