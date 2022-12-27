be69d15c43e5018e436340c335379fd9c8465112
Victor Lima <limaeriko48@gmail.com>
2022-12-27 15:24:14 -0300

    02

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-// get filter element
-// get cards elements
+const filter = document.querySelector('header input')
 
-// add input event for the filter element
+const cards = document.querySelectorAll('main ul li')
 
-// filter function
-  // if the filter is not empty
-    // for each card of cards
-      // get card heading (title)
-      // tranform to lower case
-      // transform filter text to lower case
-      // if card title does not include the filter text
-        // hide the card element
-      // else
-        // unhide the card element
-  // else
-    // for each card of cards
-      // unhide the card element
+filter.addEventListener('input', filterCards)
+
+function filterCards(){
+
+  if(filter.value != ''){
+
+    for(let card of cards){
+      let title = card.querySelector('h3')
+      title = title.textContent.toLowerCase()
+      let filterText = filter.value.toLowerCase()
+
+      if(!title.includes(filterText)){
+        card.style.display = "none"
+      }
+      else{
+        card.style.display = "block"
+      }
+    }
+  } 
+  else{
+    for(let card of cards){
+      card.style.display = "block"
+    }
+  }
+} 
