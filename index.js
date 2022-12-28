b1cf0f1b1e397fb21ec8d4f4d3bb2c458b8eb8f2
Victor Lima <limaeriko48@gmail.com>
2022-12-28 15:17:21 -0300

    x01

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const filter = document.querySelector('header input')
 
 const cards = document.querySelectorAll('main ul li')
 
+const ul = document.querySelector('main ul')
+
+const li = document.createElement("li")
+const h4 = document.createElement("h4")
+h4.innerText = "Seleção não encontrada! :("
+
+li.appendChild(h4)
+ul.appendChild(li)
+
+li.style.display = "none"
+
 filter.addEventListener('input', filterCards)
 
 function filterCards(){
-
+  let counter = 0
   if(filter.value != ''){
 
     for(let card of cards){
@@ -18,12 +29,21 @@ function filterCards(){
       }
       else{
         card.style.display = "block"
+        counter += 1
       }
+    }
+    if(counter == "0"){
+      ul.style.gridTemplateColumns = "2fr"
+      li.style.backgroundColor = "#0A1424"
+      li.style.display = "block"
+
     }
   } 
   else{
     for(let card of cards){
       card.style.display = "block"
     }
+    ul.style.gridTemplateColumns = "1fr 1fr"
+    li.style.display = "none"
   }
 } 
